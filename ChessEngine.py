c4872cbd95495a8235b321c02cb8826a5d2b2879
Enriquefft <enriquefft2001@gmail.com>
2024-08-27 01:33:40 -0500

    refactor: migrated to stateless design

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,104 +1,31 @@
-import random
+"""Module for interacting with the chess."""
+
+import secrets
 from typing import Optional, cast
 
 import chess
 
 OPENING_PHASE_LIMIT = 10
 
-class Engine:
-    def __init__(self, max_depth: int, color: chess.Color)-> None:
-        """
-        Initialize the chess engine.
 
-        Args:
-        ----
-            board (chess.Board): The chess board.
-            max_depth (int): Maximum search depth.
-            color (chess.Color): The color of the engine's pieces.
+def evaluate_board(board: chess.Board, color: chess.Color) -> float:
+    """
+    Evaluate the board using various heuristics.
 
-        """
-        self.board = chess.Board()
-        self.color = color
-        self.max_depth = max_depth
+    Args:
+    ----
+        board (chess.Board): The current state of the chess board.
+        color (chess.Color): The color to evaluate the board for.
 
-    def add_move(self, move: str) -> chess.Move:
-        """Add a move to the board."""
-        return self.board.push_san(move)
-    def undo_move(self) -> chess.Move:
-        """Undo the last move."""
-        return self.board.pop()
+    Returns:
+    -------
+        float: The evaluation score of the board.
 
+    """
 
-
-    def get_best_move(self) -> Optional[chess.Move]:
-        """
-        Get the best move for the engine.
-
-        Returns
-        -------
-            Optional[chess.Move]: The best move determined by the engine.
-
-        """
-        return cast(chess.Move, self.engine(None, 1))
-
-    def eval_function(self) -> float:
-        """
-        Evaluate the board using various heuristics.
-
-        Returns
-        -------
-            float: The evaluation score of the board.
-
-        """
-        compt = 0
-        # Sums up the material values
-        for square in chess.SQUARES:
-            compt += self.square_res_points(square)
-        compt += self.mate_opportunity() + self.opening() + 0.001 * random.random()
-        return compt
-
-    def mate_opportunity(self) -> float:
-        """
-        Check for mate opportunities.
-
-        Returns
-        -------
-            float: Mate opportunity score.
-
-        """
-        if not self.board.legal_moves:
-            return -999 if self.board.turn == self.color else 999
-        return 0
-
-    def opening(self) -> float:
-        """
-        Encourages development in the opening phase of the game.
-
-        Returns
-        -------
-            float: Opening development score.
-
-        """
-        if self.board.fullmove_number < OPENING_PHASE_LIMIT:
-            modifier = 1 / 30 * self.board.legal_moves.count()
-            return modifier if self.board.turn == self.color else -modifier
-        return 0
-
-    def square_res_points(self, square: chess.Square) -> float:
-        """
-        Return the Hans Berliner's system value of the resident piece at the given square.
-
-        Args:
-        ----
-            square (chess.Square): The square to evaluate.
-
-        Returns:
-        -------
-            float: The value of the resident piece.
-
-        """
+    def square_res_points(square: chess.Square) -> float:
         piece_value = 0
-        piece = self.board.piece_type_at(square)
+        piece = board.piece_type_at(square)
         if piece == chess.PAWN:
             piece_value = 1
         elif piece == chess.ROOK:
@@ -110,59 +37,102 @@ class Engine:
         elif piece == chess.QUEEN:
             piece_value = 8.8
 
-        return piece_value if self.board.color_at(square) == self.color else -piece_value
+        return piece_value if board.color_at(square) == color else -piece_value
 
-    def engine(self, candidate: Optional[float], depth: int) -> float| Optional[chess.Move]:
-        """
-        run the minimax algorithm with alpha-beta pruning.
+    def mate_opportunity() -> float:
+        if not board.legal_moves:
+            return -999 if board.turn == color else 999
+        return 0
 
-        Args:
-        ----
-            candidate (Optional[float]): The candidate score to compare against.
-            depth (int): The current search depth.
+    def opening() -> float:
+        if board.fullmove_number < OPENING_PHASE_LIMIT:
+            modifier = 1 / 30 * board.legal_moves.count()
+            return modifier if board.turn == color else -modifier
+        return 0
 
-        Returns:
-        -------
-            Union[float, chess.Move]: The best move or the evaluation score.
+    score = sum(square_res_points(square) for square in chess.SQUARES)
+    score += mate_opportunity()
+    score += opening()
+    score += 0.001 * secrets.randbelow(1000) / 1000.0
 
-        """
-        # Reached max depth of search or no possible moves
-        if depth == self.max_depth or not self.board.legal_moves:
-            return self.eval_function()
+    return score
 
-        # Get list of legal moves of the current position
-        move_list = list(self.board.legal_moves)
 
-        # Initialize new candidate
-        new_candidate = float("-inf") if depth % 2 != 0 else float("inf")
-        best_move = None
+def minimax(
+    board: chess.Board,
+    depth: int,
+    max_depth: int,
+    color: chess.Color,
+    candidate: Optional[float] = None,
+) -> float | Optional[chess.Move]:
+    """
+    Run the minimax algorithm with alpha-beta pruning.
 
-        # Analyze board after deeper moves
-        for move in move_list:
-            # Play move
-            self.board.push(move)
+    Args:
+    ----
+        board (chess.Board): The current state of the chess board.
+        depth (int): The current search depth.
+        max_depth (int): The maximum depth to search.
+        color (chess.Color): The color of the player to move.
+        candidate (Optional[float]): The candidate score to compare against.
 
-            # Get value of move (by exploring the repercussions)
-            value = cast(float, self.engine(new_candidate, depth + 1))
+    Returns:
+    -------
+        Union[float, chess.Move]: The best move or the evaluation score.
 
-            # Basic minimax algorithm
-            if depth % 2 != 0:  # Maximizing (engine's turn)
-                if value > new_candidate:
-                    if depth == 1:
-                        best_move = move
-                    new_candidate = value
-            elif value < new_candidate:
+    """
+    if depth == max_depth or not board.legal_moves:
+        return evaluate_board(board, color)
+
+    move_list = list(board.legal_moves)
+    new_candidate = float("-inf") if depth % 2 != 0 else float("inf")
+    best_move = None
+
+    for move in move_list:
+        board.push(move)
+        value = cast(float, minimax(board, depth + 1, max_depth, color, new_candidate))
+
+        if depth % 2 != 0:  # Maximizing (engine's turn)
+            if value > new_candidate:
+                if depth == 1:
+                    best_move = move
                 new_candidate = value
+        elif value < new_candidate:
+            new_candidate = value
 
-            # Alpha-beta pruning cuts
-            if candidate is not None and (
-                (value < candidate and depth % 2 == 0) or (value > candidate and depth % 2 != 0)
-            ):
-                self.board.pop()
-                break
+        if candidate is not None and (
+            (value < candidate and depth % 2 == 0)
+            or (value > candidate and depth % 2 != 0)
+        ):
+            board.pop()
+            break
 
-            # Undo last move
-            self.board.pop()
+        board.pop()
 
-        # Return result
-        return new_candidate if depth > 1 else best_move
+    return new_candidate if depth > 1 else best_move
+
+
+
+def get_best_move(
+    board_fen: str,
+    color: chess.Color,
+    max_depth: int,
+) -> Optional[chess.Move]:
+    """
+    Get the best move for a given board state, color, and search depth.
+
+    Args:
+    ----
+        board_fen (str): The current state of the chess board in fen format.
+            ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        color (chess.Color): The color of the player to move.
+        max_depth (int): The maximum depth to search.
+
+    Returns:
+    -------
+        Optional[chess.Move]: The best move determined by the function.
+
+
+    """
+    board = chess.Board(fen=board_fen)
+    return cast(chess.Move, minimax(board, 1, max_depth, color))
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 """Chess api main module."""
 
-from typing import Literal, Optional
+from typing import Literal, Optional, Annotated
 
-from chess import BLACK, WHITE, Move
-from fastapi import FastAPI, HTTPException
-
-from Game import Game
+from chess import Move, WHITE, BLACK
+from fastapi import FastAPI, Query
+from ChessEngine import get_best_move
 
 app = FastAPI()
-
-
-Color = Literal["white", "black"]
-
-user_games: dict[str, Game] = {}
 
 
 @app.get("/health")
@@ -21,30 +15,18 @@ async def health() -> dict[str, str]:
     return {"Hello": "World"}
 
 
-@app.get("/start")
-async def start(
-    user_id: str,
-    user_color: Color,
+Color = Literal["white", "black"]
+
+
+@app.get("/move/{depth}")
+async def get_move(
+    depth: int,
+    color: Annotated[Color, Query(None, description="The color of the player to move")],
+    fen: Annotated[
+        str,
+        Query(None, description="The FEN string representing the chess board state"),
+    ],
 ) -> Optional[Move]:
-    """Start a new game for the user."""
-    ch_color = WHITE if user_color == "white" else BLACK
-    user_games[user_id] = Game(ch_color)
-    if ch_color == BLACK:
-        return user_games[user_id].play_engine_move()
-    return None
-
-
-@app.get("/play")
-async def play_user(
-    user_id: str,
-    move: str,
-) -> Move:
     """Play a move for the user and get the engine move."""
-    if user_id not in user_games:
-        raise HTTPException(status_code=404, detail="User not found")
-    user_games[user_id].play_human_move(move)
-    bot_move = user_games[user_id].play_engine_move()
-
-    if bot_move is None:
-        raise HTTPException(status_code=404, detail="Bot move not found")
-    return bot_move
+    chess_color = WHITE if color == "white" else BLACK
+    return get_best_move(fen, chess_color, depth)
